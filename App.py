b53ffa73596fc1812618b094cecbd55c578cff7b
Tuntoine <nguyentuananh20112004@gmail.com>
2025-11-07 16:01:14 +0700

    Fix

--- App.py
+++ App.py
@@ -35,7 +35,7 @@ if not api_key:
 
 try:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-flash-lastest')
+    model = genai.GenerativeModel('gemini-flash-latest')
     st.sidebar.success("Đã kết nối với API Key!")
 except Exception as e:
     st.sidebar.error(f"Lỗi kết nối API: {e}")
